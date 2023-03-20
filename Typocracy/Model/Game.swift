f7ab8f82cbeeac191e8390e22ab98ccf1c274fc1
Antonio <antonio.saracino83@gmail.com>
2023-03-20 22:16:29 +0100

    first commit

---
+++ Typocracy/Model/Game.swift
@@ -0,0 +1,17 @@
+//
+//  Game.swift
+//  Typocracy
+//
+//  Created by Antonio on 2023-03-20.
+//
+
+import Foundation
+
+class Game {
+    var score : Int
+    
+    init(score: Int) {
+        self.score = score
+
+    }
+}
---
+++ Typocracy/Model/WordList.swift
@@ -0,0 +1,37 @@
+//
+//  WordList.swift
+//  Typocracy
+//
+//  Created by Antonio on 2023-03-20.
+//
+
+import Foundation
+
+class WordList {
+
+    private var stringList = [String]()
+    
+    init() {
+        stringList.append("pear")
+        stringList.append("apple")
+        stringList.append("kiwi")
+        stringList.append("papaja")
+        stringList.append("avocado")
+    }
+    
+    func pickedLanguage() {
+        
+    }
+    
+    func easy() {
+        
+    }
+    
+    func medium() {
+        
+    }
+    
+    func hard() {
+        
+    }
+}
