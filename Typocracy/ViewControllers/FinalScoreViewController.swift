3faf2a91c78e85fba3d38c1039f5965bbda4bd65
Antonio <antonio.saracino83@gmail.com>
2023-03-21 11:25:44 +0100

    Main views added.

---
+++ Typocracy/ViewControllers/FinalScoreViewController.swift
@@ -0,0 +1,29 @@
+//
+//  FinalScoreViewController.swift
+//  Typocracy
+//
+//  Created by Antonio on 2023-03-21.
+//
+
+import UIKit
+
+class FinalScoreViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
