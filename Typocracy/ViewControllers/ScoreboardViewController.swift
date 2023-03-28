c59deed704e03b964b3d9253b9fb59e8621a8a62
Antonio <antonio.saracino83@gmail.com>
2023-03-28 05:04:12 +0200

    User Defaults somewhat working. Added debug info

--- Typocracy/ViewControllers/ScoreboardViewController.swift
+++ Typocracy/ViewControllers/ScoreboardViewController.swift
@@ -10,24 +10,80 @@ import UIKit
 class ScoreboardViewController: UITableViewController {
     
     
-    var scoreboard = [ScoreboardEntry]()
+    //var scoreboard = [ScoreboardEntry]()
+    //var defaults = UserDefaults.standard
+    /*var scoreboard = [[String: Any]]() {
+        didSet {
+            saveScoreboard()
+        }
+    }*/
+    
+    var scoreboard = [[String: Any]]()
+    
+    private let defaults = UserDefaults.standard
+    
     var entryName : String?
     var entryScore : Int?
+    
+  
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Load the scoreboard from UserDefaults
+        print("Player name transported to scoreboard vc: \(entryName!)")
+        
+        addEntry(name: "John", score: 4)
+        addEntry(name: "Mary", score: 10)
+        addEntry(name: "Sting", score: 1)
+                
+        if let name = entryName, let score = entryScore {
+             addEntry(name: name, score: score)
+         }
+        
+        
+        loadScoreboard()
+        tableView.reloadData()
+        
+        print(scoreboard)
+        
+        /*// Load the scoreboard from UserDefaults
         if let savedScoreboard = UserDefaults.standard.object(forKey: "scoreboard") as? Data,
             let decodedScoreboard = try? JSONDecoder().decode([ScoreboardEntry].self, from: savedScoreboard) {
             scoreboard = decodedScoreboard
-        }
+        }*/
+        
+        //dict?.updateValue(entryScore!, forKey: entryName!)
 
         
-        scoreboard.sort { $0.score! > $1.score! }
+        //scoreboard.sort { $0.score! > $1.score! }
     }
     
-    override func viewWillDisappear(_ animated: Bool) {
+    private func loadScoreboard() {
+        if let savedScoreboard = defaults.object(forKey: "scoreboard") as? [[String: Any]] {
+            scoreboard = savedScoreboard
+        } else {
+            scoreboard = []
+        }
+    }
+    
+    private func saveScoreboard() {
+        defaults.set(scoreboard, forKey: "scoreboard")
+    }
+
+    func addEntry(name: String, score: Int) {
+        /*let entry = ["name" : name, "score" : score] as [String : Any]
+        scoreboard.append(entry)
+        scoreboard.sort { ($0["score"] as? Int ?? 0) > ($1["score"] as? Int ?? 0) }
+        tableView.reloadData()*/
+        
+        let entry = ["name" : name, "score" : score] as [String : Any]
+        scoreboard.append(entry)
+        scoreboard.sort { ($0["score"] as? Int ?? 0) > ($1["score"] as? Int ?? 0) }
+        tableView.reloadData()
+    }
+    
+    
+    /*override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         
         // Save the scoreboard to UserDefaults
@@ -56,7 +112,7 @@ class ScoreboardViewController: UITableViewController {
         }
         
         tableView.reloadData()
-    }
+    }*/
 
     // MARK: - Table view data source
 
@@ -72,13 +128,23 @@ class ScoreboardViewController: UITableViewController {
 
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
         let cell = tableView.dequeueReusableCell(withIdentifier: "scoreEntryCell", for: indexPath)
+        let entry = scoreboard[indexPath.row]
+        let name = entry["name"] as? String ?? "N/A"
+        let score = entry["score"] as? Int ?? 0
+        
+        cell.textLabel?.text = "\(name)     \(score)"
+        
+        /*let cell = tableView.dequeueReusableCell(withIdentifier: "scoreEntryCell", for: indexPath)
+        dict?.updateValue(entryScore!, forKey: entryName!)
+
         
         if let name = scoreboard[indexPath.row].name, let score = scoreboard[indexPath.row].score {
             cell.textLabel?.text = name + "     " + String(score)
         } else {
             cell.textLabel?.text = "N/A"
-        }
+        }*/
         
         return cell
     }
